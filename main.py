141e73c1627897eb9f60527d62ce286119786e38
logan-codes <loganandamuthu2005@gmail.com>
2026-02-05 00:57:01 +0530

    fix: added fastapi dependency injection

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-from fastapi import FastAPI, UploadFile, File
+from fastapi import FastAPI, UploadFile, File, Depends
+from functools import lru_cache 
 import shutil
 from services.document_ingester import Ingester
 from services.retriever import Retriever
@@ -8,28 +9,33 @@ import os
 
 app = FastAPI()
 
-ingester = Ingester()
-retriever = Retriever()
-generator = Generation()
+@lru_cache()
+def get_ingester():
+    return Ingester()
+@lru_cache()
+def get_retriever():
+    return Retriever()
+@lru_cache()
+def get_generator():
+    return Generation()
 
 @app.get("/")
 async def health_check():
     return {"status": "ok"}
 
 @app.post("/document")
-async def upload_file(file:UploadFile=File(...)):
+async def upload_file(file:UploadFile=File(...), ingester: Ingester = Depends(get_ingester)):
     os.makedirs("db/uploads", exist_ok=True)
 
     file_path = f"db/uploads/{file.filename}"
 
     with open(file_path, "wb") as buffer:
         shutil.copyfileobj(file.file, buffer)
-    
     ingester.ingest_documents(file_path)
     return {"filename": file.filename, "message": "File uploaded and ingested successfully."}
 
 @app.get("/documents")
-def list_documents():
+def list_documents(ingester: Ingester = Depends(get_ingester)):
     documents = ingester.list_documents()
     return {"documents": documents}
 
@@ -37,7 +43,7 @@ class DeleteRequest(BaseModel):
     source: str
 
 @app.delete("/document")
-def clear_document(payload: DeleteRequest):
+def clear_document(payload: DeleteRequest,ingester: Ingester = Depends(get_ingester)):
     message = ingester.delete_document(payload.source)
     return {"message": message}
 
@@ -45,7 +51,7 @@ class ChatRequest(BaseModel):
     question: str
 
 @app.post("/chat")
-async def chat_endpoint(request: ChatRequest):
+async def chat_endpoint(request: ChatRequest, retriever: Retriever = Depends(get_retriever), generator: Generation = Depends(get_generator)):
 
     context = retriever.retrieve_context(request.question)
     response = generator.generate_response(request.question, context)
